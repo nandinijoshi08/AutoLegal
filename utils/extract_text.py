039fde127ae759eeb5cb6f659bbb5ad16682074a
Nandini B Joshi <113448925+nandinijoshi08@users.noreply.github.com>
2025-11-18 17:29:07 +0530

    extract_text.py

---
+++ utils/extract_text.py
@@ -0,0 +1,11 @@
+import pdfplumber
+
+def extract_text_from_pdf(file):
+    if file:
+        with pdfplumber.open(file) as pdf:
+            all_text = ""
+            for page in pdf.pages:
+                all_text += page.extract_text() + "\n\n" 
+        return all_text 
+    else:
+        return "Please provide the file"
